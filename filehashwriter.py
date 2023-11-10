e3d5dc913572160733535a4c332da587d7ad9252
Simon Mikkelsen <simonmikkelsen@users.noreply.github.com>
2023-11-10 15:56:26 +0100

    Added minsize argument.

--- filehashwriter.py
+++ filehashwriter.py
@@ -1,17 +1,17 @@
+from os import stat_result
 from pathlib import Path
-
 
 class FileHashWriter:
     def __init__(self, basePath: Path) -> None:
         self.basePath = basePath
         self.basePath.mkdir(parents=True, exist_ok=True)
 
-    def persistHash(self, hash:str, filePath: Path) -> None:
+    def persistHash(self, hash:str, filePath: Path, stat:stat_result) -> None:
         firstChars = hash[0:2]
         with open(Path(self.basePath, firstChars + '.txt'), 'a') as f:
             f.write(hash)
             f.write(' ')
-            f.write(str(filePath.stat().st_size))
+            f.write(str(stat.st_size))
             f.write(' ')
             f.write(str(filePath.absolute()))
             f.write('\n')
--- scan.py
+++ scan.py
@@ -12,21 +12,44 @@ parser.add_argument('-p', '--path', dest ='path',
                     default = [], help ='Paths to index. Specify multiple times for more paths.')
 parser.add_argument('--hashes', dest='hashes', action='store', default='hashes',
                     help='Path to store hashes.')
+parser.add_argument('-m', '--min-size', dest='minsize', action='store', default='1',
+                    help='Minimum file size to hash. Default 1 byte (ignore empty files). Postfix with k, m, g, t, e, p, e, z or y for KiB, MiB, GiB etc.')
 args = parser.parse_args()
 
-blockSize = 200*1024
+blockSize = 50*4096
 blockCount = 2
 basepath = args.hashes
 
+if len(args.path) > 0:
+  pathList = args.path
+else:
+  pathList = ['.']
+
+def sizeToInt(size: str) -> int:
+  if (size.isnumeric()):
+    return int(size)
+  # A dictionary that maps the letters to their corresponding values
+  letter_values = {"k": 1024, "m": 1024**2, "g": 1024**3, "t": 1024**4, "p": 1024**5, "e": 1024**6, "z": 1024*7, "y": 1024**8}
+  # Split the string into the number and the letter parts
+  number = float(size[:-1])
+  letter = size[-1].lower()
+  # Check if the letter is valid
+  if letter not in letter_values:
+    raise ValueError("Invalid size postfix: " + letter)
+  # Multiply the number by the value of the letter and return it as an integer
+  return int(number * letter_values[letter])
+
+minSize = sizeToInt(args.minsize)
 filehash = BlockFileHash(blockSize, blockCount)
 writer = FileHashWriter(Path(basepath))
 
-# Travers all the branch of a specified path, bottom first
-for indexpath in args.path:
+for indexpath in pathList:
   for (root,dirs,files) in os.walk(indexpath, topdown=False):
     for file in files:
       path = Path(root, file)
       if (path.is_file()):
-        hash = filehash.getHashcode(path)
-        writer.persistHash(hash, path)
-        print(hash + ' ' + str(path.absolute()))
+        stat = path.stat()
+        if (stat.st_size >= minSize):
+          hash = filehash.getHashcode(path)
+          writer.persistHash(hash, path, stat)
+          print(hash + ' ' + str(path.absolute()))
